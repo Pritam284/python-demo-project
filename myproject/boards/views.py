bf5ea4a4ecea12c02613000ee9cc121375cbea1a
Pritam Das <pritam@asl.aero>
2019-01-08 18:02:57 +0600

    form update

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User 
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from .forms import NewTopicForm
 from .models import Board, Topic, Post
 
 def home(request):
@@ -19,26 +20,12 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
 	board = get_object_or_404(Board, pk=pk)
-
+	user = User.objects.first()
 	if request.method == 'POST':
-		subject = request.POST['subject']
-		message = request.POST['message']
-
-		user = User.objects.first()
-
-		topic = Topic.objects.create(
-			subject=subject,
-			board=board,
-			starter=user
-		)
-
-		post = Post.objects.create(
-			message=message,
-			topic=topic,
-			created_by=user 
-		)
-
-		return redirect('board_topics', pk=board.pk)
-
-
-	return render(request, 'new_topic.html', {'board': board})
+		form = NewTopicForm(request.POST)
+		if form.is_valid():
+			topic = form.save()
+			return redirect('board_topics', pk=board.pk)
+	else:
+		form = NewTopicForm()
+	return render(request, 'new_topic.html', {'form': form})
